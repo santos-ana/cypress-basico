096c00ccf46199c7199a90c94257e374a985ca79
santos-ana <firmino.anac@gmail.com>
2023-03-05 15:49:01 -0300

    adicionado comando cypress

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -1,53 +1,78 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
+describe('Central de Atendimento ao Cliente TAT', function () {
 
-  beforeEach (() => {
+  beforeEach(() => {
     cy.visit('./src/index.html')
   })
 
-  it('verifica o título da aplicação', function() {
+  it('verifica o título da aplicação', function () {
     cy.title()
-    .should('be.equal', 'Central de Atendimento ao Cliente TAT')
+      .should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
 
-  it('preenche os campos obrigatórios e envia o formulário', function() {
-    cy.get('[data-test="campoNome"]')
-    .type('Ana')
-    cy.get('[data-test="campoSobrenome"]')
-    .type('Santos')
+  it('preenche os campos obrigatórios e envia o formulário', function () {
     cy.get('[data-test="campoEmail"]')
-    .type('firmino.anac@gmail.com')
+      .type('firmino.anac@gmail.com')
     cy.get('[data-test="campoTelefone"]')
-    .type('85986933977')
-    cy.get('[data-test="campoDescricao"]')
-    .type('Não encontrei a apostila do curso. Onde posso encontrá-la?')
-    cy.get("[data-test=botaoEnviar")
-    .click()
+      .type('99999999999')
+    cy.fillMandatoryFieldsAndSubmit()
     cy.get(".success")
-    .should("be.visible")
+      .should("be.visible")
   })
 
-  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
-    cy.get('[data-test="campoNome"]')
-    .type('Ana')
-    cy.get('[data-test="campoSobrenome"]')
-    .type('Santos')
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
     cy.get('[data-test="campoEmail"]')
-    .type('firmino.anac.gmail.com')
+      .type('firmino.anac.gmail.com')
     cy.get('[data-test="campoTelefone"]')
-    .type('85986933977')
-    cy.get('[data-test="campoDescricao"]')
-    .type('Não encontrei a apostila do curso. Onde posso encontrá-la?')
-    cy.get("[data-test=botaoEnviar")
-    .click()
+      .type('99999999999')
+    cy.fillMandatoryFieldsAndSubmit()
     cy.get(".error")
-    .should("be.visible")
+      .should("be.visible")
   })
 
-  it('validar se que o campo telefone fica vazio ao ser digitado um valor não-numérico', function() {
+  it('valida se o campo telefone fica vazio ao ser digitado um valor não-numérico', function () {
     cy.get('[data-test="campoTelefone"]')
-    .type('Ana')
-    .should('be.empty')
+      .type('Ana')
+      .should('be.empty')
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
+    cy.get('[data-test="campoEmail"]')
+      .type('firmino.anac@gmail.com')
+    cy.get("input[id='phone-checkbox']").check()
+    cy.fillMandatoryFieldsAndSubmit()
+    cy.get(".error")
+      .should("be.visible")
+  })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', function () {
+    cy.get('[data-test="campoNome"]')
+      .type('Ana')
+      .should('have.value', 'Ana')
+      .clear()
+      .should('be.empty')
+    cy.get('[data-test="campoSobrenome"]')
+      .type('Santos')
+      .should('have.value', 'Santos')
+      .clear()
+      .should('be.empty')
+    cy.get('[data-test="campoEmail"]')
+      .type('firmino.anac@gmail.com')
+      .should('have.value', 'firmino.anac@gmail.com')
+      .clear()
+      .should('be.empty')
+    cy.get('[data-test="campoTelefone"]')
+      .type('99999999999')
+      .should('have.value', '99999999999')
+      .clear()
+      .should('be.empty')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
+    cy.contains('button', 'Enviar')
+        .click()
+    cy.get(".error")
+      .should("be.visible")
   })
 })
