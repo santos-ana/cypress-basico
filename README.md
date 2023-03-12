eeea393c1fe5c8c35a12ceed19ac74d926cd3090
santos-ana <firmino.anac@gmail.com>
2023-03-12 17:37:57 -0300

    adicionado comandos de upload e documentação do repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# cypress-basico
+
+## Pre-requirements
+
+It is required to have Node.js and npm installed to run this project.
+
+> I used versions `v16.13.2` and `8.3.2` of Node.js and npm, respectively. I suggest you use the same or later versions.
+
+## Installation
+
+Run `npm install` (or `npm i` for the short version) to install the dev dependencies.
+
+## Tests
+
+Run `npm test` (or `npm t` for the short version) to run the test in headless mode.
+
+Or, run `npm run cy:open` to open Cypress in interactive mode.
+
--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -112,4 +112,34 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .uncheck()
       .should('not.be.checked')
   })
+
+  it('seleciona um arquivo da pasta fixtures', function () {
+    cy.get('input[type="file"]')
+      .should('not.have.value')
+      .selectFile('cypress/fixtures/arquivo.json')
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal('arquivo.json')
+      })
+  })
+
+  it('seleciona um arquivo simulando um drag-and-drop', function () {
+    cy.fixture('arquivo.json').as('arquivoTeste')
+    cy.get('input[type="file"]')
+      .should('not.have.value')
+      .selectFile('@arquivoTeste', { action: 'drag-drop' })
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal('arquivo.json')
+      })
+  })
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function () {
+    cy.get('#privacy a')
+      .should('have.attr', 'target', '_blank')
+  })
+
+  it.only('acessa a página da política de privacidade removendo o target e então clicando no link', function () {
+    cy.get('#privacy a')
+      .invoke('removeAttr', 'target')
+      .click()
+  })
 })
